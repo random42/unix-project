456ebe10cf749d55f0fb85b3d6401d2b47e5cda3
Roberto Sero <roberto.sero42@gmail.com>
2018-02-05 00:16:15 +0100

    added includes

--- src/gestore.c
+++ src/gestore.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <sys/wait.h>
 #include <sys/types.h>
 #include <signal.h>
 #include <stdio.h>
--- src/people.c
+++ src/people.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include "header.h"
 #include "people.h"
 
